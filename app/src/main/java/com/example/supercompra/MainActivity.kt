74718b445a75f3f3a2d52e65553570d08c013698
Mateus <limamateus67@gmail.com>
2025-08-19 21:57:33 -0300

    Projeto Inicial

---
+++ app/src/main/java/com/example/supercompra/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.supercompra
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.supercompra.ui.theme.SuperCompraTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SuperCompraTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                  Titulo(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+// Componente
+@Composable
+fun Titulo(modifier: Modifier = Modifier) {
+    Text(text = "Lista de Compras",modifier = modifier)
+
+}
+
+@Preview
+@Composable
+private fun TituloPreview() {
+    Titulo()
+}
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SuperCompraTheme {
+        Greeting("Android")
+    }
+}
