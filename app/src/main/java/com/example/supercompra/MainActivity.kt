06b60d10b6c1f8e523802d38100b8ec2c369d3af
Mateus-R-De-Lima <limamateus67@gmail.com>
2025-08-19 23:38:10 -0300

    feat & refactor: criar componente de item de lista e refatorar título para texto dinâmico
    Feat: Criado um novo componente que representa um item individual dentro de uma lista. Esse item poderá ser reutilizado com diferentes conteúdos, facilitando a criação de listas dinâmicas na UI com Jetpack Compose.
    
    Refactor: Refatorado o componente de título (Title) para receber um texto como parâmetro, permitindo a reutilização com diferentes conteúdos textuais, ao invés de manter o texto fixo.

--- app/src/main/java/com/example/supercompra/MainActivity.kt
+++ app/src/main/java/com/example/supercompra/MainActivity.kt
@@ -5,22 +5,26 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.supercompra.ui.theme.Marinho
 import com.example.supercompra.ui.theme.SuperCompraTheme
 import com.example.supercompra.ui.theme.Typography
 
@@ -31,21 +35,28 @@ class MainActivity : ComponentActivity() {
         setContent {
             SuperCompraTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                  //ImagemTopo(modifier = Modifier.padding(innerPadding))
-                 // Icone(Icons.Default.Add,modifier = Modifier.padding(innerPadding))
-                  Titulo(modifier = Modifier.padding(innerPadding))
+                        Column {
+                            ImagemTopo(modifier = Modifier.padding(innerPadding))
+                            Titulo(texto = "Lista de Compras", Modifier.padding(innerPadding))
+                            ItemDaLista(modifier = Modifier.padding(innerPadding))
+                            Titulo(texto = "Comprados", Modifier.padding(innerPadding))
+                        }
+
+
+
                 }
             }
         }
     }
 }
+
 // Componente
 @Composable
-fun Titulo(modifier: Modifier = Modifier) {
+fun Titulo(texto: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Lista de Compras",
+        text = texto,
+        style = Typography.headlineLarge,
         modifier = modifier,
-        style = Typography.headlineLarge
     )
 
 }
@@ -53,7 +64,7 @@ fun Titulo(modifier: Modifier = Modifier) {
 @Preview
 @Composable
 private fun TituloPreview() {
-    Titulo()
+    Titulo(texto = "Lista de Compras")
 }
 
 
@@ -69,28 +80,59 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun ImagemTopo(modifier: Modifier = Modifier) {
     Image(
         painter = painterResource(
-            R.drawable.logo_top_supercompra),
+            R.drawable.logo_top_supercompra
+        ),
         contentDescription = "Logo",
-        modifier = modifier.size(160.dp))
+        modifier = modifier.size(160.dp)
+    )
 }
 
 @Preview
 @Composable
 private fun ImagemTopoPreview() {
-   SuperCompraTheme {
-       ImagemTopo()
-   }
+    SuperCompraTheme {
+        ImagemTopo()
+    }
 }
 
 @Composable
-fun Icone(icone: ImageVector,modifier: Modifier = Modifier) {
-    Icon(icone, contentDescription = "Editar",modifier = modifier)
+fun Icone(icone: ImageVector, modifier: Modifier = Modifier) {
+    Icon(icone, contentDescription = "Editar", modifier = modifier,tint = Marinho)
 }
 
 @Preview
 @Composable
 private fun IconeEditePreview() {
     Icone(Icons.Default.Edit)
+}
+
+@Composable
+fun ItemDaLista(modifier: Modifier = Modifier) {
+    Row {
+        Checkbox(
+            checked = false,
+            onCheckedChange = {},
+            modifier = modifier.padding(end = 8.dp)
+        )
+        Text(
+            text = "Suco",
+            style = Typography.bodyMedium,
+            textAlign = TextAlign.Center,
+            modifier = modifier
+
+        )
+        Icone(Icons.Default.Delete, modifier.size(16.dp))
+        Icone(Icons.Default.Edit,modifier.size(16.dp))
+    }
+
+}
+
+@Preview
+@Composable
+private fun ItemDaListaPreview() {
+    SuperCompraTheme {
+        ItemDaLista()
+    }
 }
 
 @Preview(showBackground = true)
