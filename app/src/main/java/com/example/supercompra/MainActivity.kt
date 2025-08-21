c16641f9c1f77197aec194d0167b24afb938bafa
Mateus-R-De-Lima <limamateus67@gmail.com>
2025-08-20 21:26:26 -0300

    feat: implementa OutlinedTextField para adicionar itens dinamicamente na linha
    Adicionado um OutlinedTextField para entrada de texto. O valor digitado é controlado por:
    var texto = rememberSaveable { mutableStateOf("") }
    O campo é atualizado em tempo real com onValueChange = { texto.value = it }, permitindo adicionar itens dinamicamente. O uso de rememberSaveable garante que o valor seja mantido mesmo após recomposições ou mudanças de configuração.

--- app/src/main/java/com/example/supercompra/MainActivity.kt
+++ app/src/main/java/com/example/supercompra/MainActivity.kt
@@ -9,19 +9,27 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
@@ -37,18 +45,22 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SuperCompraTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                        Column(
-                            verticalArrangement = Arrangement.Top
+                Scaffold(
+                    modifier = Modifier.fillMaxSize().padding(16.dp)
+                )
+                { innerPadding ->
+                    Column(
+                        verticalArrangement = Arrangement.Top,
+                        modifier = Modifier.padding(innerPadding)
 
-                        )
-                        {
-                            ImagemTopo(modifier = Modifier.padding(innerPadding))
-                            Titulo(texto = "Lista de Compras", Modifier.padding(innerPadding))
-                            ItemDaLista(modifier = Modifier.padding(innerPadding))
-                            Titulo(texto = "Comprados", Modifier.padding(innerPadding))
-                        }
-
+                    )
+                    {
+                        ImagemTopo()
+                        AdicionarItem()
+                        Titulo(texto = "Lista de Compras")
+                        ItemDaLista()
+                        Titulo(texto = "Comprados")
+                    }
 
 
                 }
@@ -104,7 +116,7 @@ private fun ImagemTopoPreview() {
 
 @Composable
 fun Icone(icone: ImageVector, modifier: Modifier = Modifier) {
-    Icon(icone, contentDescription = "Editar", modifier = modifier,tint = Marinho)
+    Icon(icone, contentDescription = "Editar", modifier = modifier, tint = Marinho)
 }
 
 @Preview
@@ -168,6 +180,38 @@ fun ItemDaLista(modifier: Modifier = Modifier) {
 private fun ItemDaListaPreview() {
     SuperCompraTheme {
         ItemDaLista()
+    }
+}
+
+
+@Composable
+fun AdicionarItem(modifier: Modifier = Modifier) {
+    var texto = rememberSaveable() { mutableStateOf("") }
+
+    OutlinedTextField(
+        value = texto.value,
+        onValueChange = { texto.value = it },
+        placeholder =
+            {
+                Text(
+                    "Digite o item que deseja adicionar",
+                    color = Color.Gray,
+                    style = Typography.bodyMedium
+                )
+            },
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        singleLine = true,
+        shape = RoundedCornerShape(24.dp)
+    )
+}
+
+@Preview
+@Composable
+private fun AdicionarItemPreview() {
+    SuperCompraTheme {
+        AdicionarItem()
     }
 }
 
